86e30631089da45c376c476b4c3f56a503b5056c
Nhien <63007332+tinieee@users.noreply.github.com>
2021-10-15 15:36:52 +0700

    Bài làm
    File bài làm

---
+++ app/src/main/java/com/example/a25_16028881_nguyenngocnhien/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a25_16028881_nguyenngocnhien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
